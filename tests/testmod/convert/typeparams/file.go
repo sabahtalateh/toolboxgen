e5ec5008a5f05c3d4c391920d91011e7f07da728
sabahtalateh <sabahtalateh@gmail.com>
2023-07-28 00:53:08 +0700

    wip

--- tests/testmod/convert/typeparams/file.go
+++ tests/testmod/convert/typeparams/file.go
@@ -4,11 +4,11 @@ type C[A, B any] *interface {
 	F(a A) (B, error)
 }
 
-// type A[X, Y, Z any] struct {
-// 	f  *X
-// 	f2 []C[Y, uint]
-// 	f3 *Z
-// }
+type A[X, Y, Z any] struct {
+	f  *X
+	f2 []C[Y, uint]
+	f3 *Z
+}
 
 // type B[T any] A[[]string, []T, []struct{ a A[**T, struct{}, **T] }]
 
