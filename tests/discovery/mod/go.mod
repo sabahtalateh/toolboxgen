d65480d028814521ff182f3b9c2460867e36ec4d
sabahtalateh <sabahtalateh@gmail.com>
2023-07-29 20:18:23 +0700

    wip

--- tests/discovery/mod/go.mod
+++ tests/discovery/mod/go.mod
@@ -1,3 +1,5 @@
 module mod
 
 go 1.20
+
+require github.com/sabahtalateh/toolbox v0.0.0-20230729130607-a34ede16d634
--- tests/discovery/mod/services/mailing.go
+++ tests/discovery/mod/services/mailing.go
@@ -1,6 +1,12 @@
 package services
 
+import (
+	"github.com/sabahtalateh/toolbox/di"
+)
+
 type Mailing struct{}
+
+var _ = di.Component()
 
 func NewMailing() *Mailing {
 	return &Mailing{}
